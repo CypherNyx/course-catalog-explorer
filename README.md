8abe40deccbf1fa5dd10f7d2e17f6f3565e008b7
Dahlia Guido <dguido.dev@gmail.com>
2024-04-10 12:20:26 -0500

    adding initial logo and color scheme

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Angular application for a  searchable and filterable interface to display availa
 [>> Figjam Wireframe and sitemap](https://www.figma.com/file/DpIHqkyC1os8P89cga7ioB/Wireframe-Planning-for-Udemy-Compass?type=whiteboard&node-id=0%3A1&t=8lBXhu0EIq70imaK-1);
 
 ![wireframe home version 1](./src/assets/wireframe_home_v1)
+
+
+## Attributions
+<a href="https://www.freepik.com/free-vector/flat-design-advertising-agency-logo-template_36295619.htm#fromView=image_search_similar&page=1&position=0&uuid=da45d1de-480f-41d2-bc94-1200f5250449">Image by freepik</a>
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainContentComponent,
-    FooterComponent
+    FooterComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
